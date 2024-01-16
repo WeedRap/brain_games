ad0bd6e2a1b16c322a3a120992d09bc18a8992cb
WeedRap <1337rafik.weed@gmail.com>
2024-01-16 22:56:45 +0300

    Use gcd from num::integer instead my func

--- src/games/gcd.rs
+++ src/games/gcd.rs
@@ -1,20 +1,12 @@
 use crate::utils::get_rand_num;
-
-fn calculate_gcd(mut num1: u8, mut num2: u8) -> u8 {
-    while num2 != 0 {
-        let tmp = num2;
-        num2 = num1 % num2;
-        num1 = tmp;
-    }
-    num1
-}
+use num::integer::gcd;
 
 pub fn get_nums_pair_and_gcd() -> (String, String) {
     let num1: u8 = get_rand_num(1, 10);
     let num2: u8 = get_rand_num(1, 10);
 
     let nums_pair: String = format!("{} {}", num1, num2);
-    let gcd: u8 = calculate_gcd(num1, num2);
+    let gcd: u8 = gcd(num1, num2);
 
     (nums_pair, gcd.to_string())
 }
